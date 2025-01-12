e78ecea6b1ee34a9f0adca012a73b564bb9fa5a1
romuloban <romulo88@hotmail.com>
2025-01-12 20:10:12 +0100

    add cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { StrictMode } from 'react';
+import { StrictMode, use, useState } from 'react';
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from './PizzaOfTheDay';
 import Order from "./Order";
+import Header from './Header';
+import { CartContext } from './contexts';
 
 const App = () => {
+  const cartHook = useState([]);
   return (
     <StrictMode>
-      <div>
-        <h1 className='logo'>padre gino's - order now</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 };
