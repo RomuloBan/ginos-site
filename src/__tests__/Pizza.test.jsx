4364d01a0fdd81adcf1602dbb0f66fe0fb93b872
romuloban <romulo88@hotmail.com>
2025-01-26 22:56:29 +0100

    add pizza alt img test

---
+++ src/__tests__/Pizza.test.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import Pizza from '../Pizza';
+
+test('alt text renders on pizza image', async () => {
+  const name = 'My Favorite Pizza';
+  const src = 'https://picsum.photos/200';
+  const screen = render(
+    <Pizza name={name} description='super cool pizza' image={src} />
+  );
+
+  const img = await screen.getByRole('img');
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
